948292ae0dba6247c025ff9de0cce979f86829d2
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-10-01 09:27:58 -0700

    setting up server with ApolloServer in server/server.js

--- server/server.js
+++ server/server.js
@@ -8,6 +8,16 @@ const { ApolloServer } = require('apollo-server-express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+server.start().then(() => {
+  server.applyMiddleware({ app });
+})
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
