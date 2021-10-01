79b4c465e78918df14ddb0f98c3268b66979876d
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-10-01 09:30:19 -0700

    Added the app.get for send the file to the build folder under index.html

--- server/server.js
+++ server/server.js
@@ -26,7 +26,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(_dirname), '../client/build/index.html');
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
