6599d98d6b71478e57e7b3588cffa29f6a60bec5
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-10-01 10:37:42 -0700

    import @apollo/client in App.js. made link constant to generate the graphql

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const link = createHttpLink({
+  uri: '/graphql',
+})
+
 
 function App() {
   return (
