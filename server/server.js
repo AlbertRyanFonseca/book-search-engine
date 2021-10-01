dd52373189e7c55d0423a3a14b756023b5e278c7
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-10-01 10:26:01 -0700

    Deleted requirement for routes, added schemas. commented out the app.get

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
 const { ApolloServer } = require('apollo-server-express');
+const { resolvers, typeDefs } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -28,9 +28,9 @@ if (process.env.NODE_ENV === 'production') {
 
 // app.use(routes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(_dirname), '../client/build/index.html');
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(_dirname), '../client/build/index.html');
+// })
 
 db.once('open', () => {
   app.listen(PORT, () => {
