7a9fb7e8399fb1c958696f7b4dc0d9520389fe76
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-10-01 09:52:53 -0700

    Added resolvers constant, and within, added the Query.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,24 @@
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const { Authentication } = require('apollo-server-express');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('savedBooks');
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('savedBooks');
+
+
+                return userData;
+            }
+            // throw new AuthenticationError('Not logged in');
+        },
+    },
+}
